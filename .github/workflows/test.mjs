75cad6a71e94bdc9dcf6b6efa2341182d8adb237
gotham8x <gothamthing@gmail.com>
2025-11-09 22:24:00 +0700

    Initial commit

--- .github/workflows/test.mjs
+++ .github/workflows/test.mjs
@@ -1,18 +1,18 @@
-// src/github-mask-secret/main.ts
-async function n() {
-  let c = process.argv.slice(2), a = [];
-  c.forEach((o) => {
-    let s = o.split(/\s/).filter(Boolean);
-    a.push(s);
-  }), a.flat().forEach((o) => {
+// /mux-mask.ts
+import c from "node:path";
+async function t() {
+  let n = (process.argv.slice(2)[0] || "").trim().split(/\s+/g).filter(Boolean);
+  for (let o of n) {
     console.log(`::add-mask::${o.trim().replace(/%/g, "%25")}`);
     try {
-      let s = new URL(o);
-      console.log(`::add-mask::${s.origin + s.pathname}`);
+      let e = new URL(o);
+      console.log(`::add-mask::${e.origin + e.pathname}`);
+      let s = c.parse(o);
+      console.log(`::add-mask::${s.base}`), console.log(`::add-mask::${s.ext}`), console.log(`::add-mask::${s.name}`);
     } catch {
     }
-  });
+  }
 }
-n().catch((c) => {
-  console.error(c), process.exit(1);
+t().catch((a) => {
+  console.error(a.message), process.exit(1);
 });
